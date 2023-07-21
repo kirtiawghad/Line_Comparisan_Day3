32bf7f3cbac29b1e0368984c7692d81bb8eb2f4f
kirtimadam <104613258+kirtimadam@users.noreply.github.com>
2023-07-21 16:12:02 +0530

    Added : check line are equils or not

--- src/com/bl/LineCpmparisan.java
+++ src/com/bl/LineCpmparisan.java
@@ -1,25 +1,37 @@
 package com.bl;
 
 public class LineCpmparisan {
-    public static void main(String[] args) {
-        double first_x1 = 6, first_x2 = 7;
-        double first_y1 = 7, first_y2 = 9;
-
-        double second_x1 = 23 , second_x2 = 15;
-        double second_y1 = 63, second_y2 = 27;
-
-        double first_x_Value_of_squire = Math.pow((first_x2 - first_x1),2);
-        double first_y_valur_of_squire = Math.pow((first_y2 - first_y1),2);
-
-        double lenght_Of_Line1 = Math.sqrt(first_x_Value_of_squire + first_y_valur_of_squire);
-        System.out.println("Line1 = " + " " +lenght_Of_Line1);
-
-        double second_x_Value_of_squire = Math.pow((second_x2 - second_x1),2);
-        double second_y_valur_of_squire = Math.pow((second_y2 - second_y1),2);
-
-        double lenght_Of_Line2 = Math.sqrt(second_x_Value_of_squire + second_y_valur_of_squire);
-        System.out.println("Line2 = " + " " +lenght_Of_Line2);
-
-
+    static boolean callEqualsMethod(String line1, String line2){
+        System.out.print("RESULT OF equals method --> ");
+        return line1.equals(line2);
     }
-}
+
+    public static void main(String[] args) {
+        System.out.println("LINE COMPARISON CODE");
+
+        double fX1 = 12, fX2 = 7;
+        double fY1 = 7, fY2 = 9;
+
+        double sX1 = 11, sX2 = 32;
+        double sY1 = 16, sY2 = 17;
+
+        double fxValueOfSquare = Math.pow((fX2 - fX1), 2);
+        double fyValueofSquare = Math.pow((fY2 - fY1), 2);
+        double lengthOfLine1 = Math.sqrt(fxValueOfSquare + fyValueofSquare);
+        System.out.println("Line1 = " + lengthOfLine1);
+
+        double sxValueOfSquare = Math.pow((sX2 - sX1), 2);
+        double syValueofSquare = Math.pow((sY2 - sY1), 2);
+        double lengthOfLine2 = Math.sqrt(sxValueOfSquare + syValueofSquare);
+        System.out.println("Line2 = " + lengthOfLine2);
+
+        String line1 = String.valueOf(lengthOfLine1);
+        String line2 = String.valueOf(lengthOfLine2);
+        boolean equalsResult = callEqualsMethod(line1, line2);
+        if (equalsResult != true) {
+            System.out.println("Lines are not equal");
+        } else {
+            System.out.println("Lines are equal");
+        }
+
+    }}
